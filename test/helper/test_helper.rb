0607aba8028f8decab7cbe8e16808cbc672ecb24
Katrin Nilsson <katrin.nilsson@klarna.com>
2014-08-14 10:27:40 +0200

    Removing id's from resources in file, since they are not needed, and adding displaytext for resources.

--- test/helper/test_helper.rb
+++ test/helper/test_helper.rb
@@ -4,14 +4,17 @@ class TestHelper
     resource_cloud = [
       { "name" => "Resource-01",
         "id" => 111,
+        "displaytext" => "Description A",
         "tag" => "RES"
       },
       { "name" => "Resource-02",
         "id" => 222,
+        "displaytext" => "Description B",
         "tag" => "RES"
       },
       { "name" => "Resource-03",
         "id" => 333,
+        "displaytext" => "Description C",
         "tag" => "RAN"
       }
     ]
@@ -21,15 +24,15 @@ class TestHelper
   def get_resource_file()
     resource_file = {
       "Resource-01" => {
-        "id" => 111,
+        "displaytext" => "A different description",
         "tag" => "RES"
       },
       "Resource-02" => {
-        "id" => 222,
+        "displaytext" => "Description B",
         "tag" => "RAN"
       },
       "Resource-04" => {
-        "id" => 444,
+        "displaytext" => "Description D",
         "tag" => "RAN"
       }
     }
